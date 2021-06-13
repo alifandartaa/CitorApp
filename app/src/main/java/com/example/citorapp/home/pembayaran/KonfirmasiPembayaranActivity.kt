721558426161eca276ce6cfce6cca822b2ac8b66
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-06-13 13:50:15 +0700

    Pembayaran

---
+++ app/src/main/java/com/example/citorapp/home/pembayaran/KonfirmasiPembayaranActivity.kt
@@ -0,0 +1,12 @@
+package com.example.citorapp.home.pembayaran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.citorapp.R
+
+class KonfirmasiPembayaranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_pembayaran)
+    }
+}
