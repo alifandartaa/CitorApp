350442df69511d13f606b6220afa02a5e6397b39
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-06-13 13:47:26 +0700

    activity pelayanan

---
+++ app/src/main/java/com/example/citorapp/home/PelayananActivity.kt
@@ -0,0 +1,23 @@
+package com.example.citorapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.citorapp.R
+import com.example.citorapp.databinding.ActivityPelayananBinding
+
+class  PelayananActivity : AppCompatActivity() {
+//    private var bindingPelayanan: ActivityPelayananBinding? = null
+//    private val binding get() = bindingPelayanan!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pelayanan)
+//        bindingPelayanan = ActivityPelayananBinding.inflate(layoutInflater)
+//
+//        val kategori = resources.getStringArray(R.array.kategori)
+//        val arrayAdapter = ArrayAdapter(this@PelayananActivity, R.layout.dropdown_item, kategori)
+//        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+    }
+}
