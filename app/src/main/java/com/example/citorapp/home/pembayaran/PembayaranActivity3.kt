b0138310f7c23bb794dbd22661d6052fb01a373c
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-06-13 13:49:42 +0700

    Wash and Detailing

---
+++ app/src/main/java/com/example/citorapp/home/pembayaran/PembayaranActivity3.kt
@@ -0,0 +1,12 @@
+package com.example.citorapp.home.pembayaran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.citorapp.R
+
+class PembayaranActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembayaran3)
+    }
+}
