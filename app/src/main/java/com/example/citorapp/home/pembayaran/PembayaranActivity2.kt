68d32b2370822f0d61a6dc7212f16a76383ac156
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-06-15 16:56:05 +0700

    Activity Pembayaran V2

--- app/src/main/java/com/example/citorapp/home/pembayaran/PembayaranActivity2.kt
+++ app/src/main/java/com/example/citorapp/home/pembayaran/PembayaranActivity2.kt
@@ -1,12 +1,28 @@
 package com.example.citorapp.home.pembayaran
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.citorapp.R
+import com.example.citorapp.databinding.ActivityPembayaran2Binding
 
 class PembayaranActivity2 : AppCompatActivity() {
+
+    private lateinit var bindingSecondPayment: ActivityPembayaran2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pembayaran2)
+        bindingSecondPayment = ActivityPembayaran2Binding.inflate(layoutInflater)
+        setContentView(bindingSecondPayment.root)
+
+
+        bindingSecondPayment.btnChooseOvo.setOnClickListener(){
+            val intent = Intent(this@PembayaranActivity2, PembayaranActivity3::class.java)
+            startActivity(intent)
+        }
+
+        bindingSecondPayment.btnChooseShopeepay.setOnClickListener(){
+            val intent = Intent(this@PembayaranActivity2, PembayaranActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
