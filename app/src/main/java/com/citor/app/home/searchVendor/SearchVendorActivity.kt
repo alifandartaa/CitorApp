fd79673cc635780b40a3c93e068756170c141a89
Alif Andarta Al Falah <alifandartaa@yahoo.com>
2021-07-17 12:40:57 +0700

    refine pick current user location

--- app/src/main/java/com/citor/app/home/searchVendor/SearchVendorActivity.kt
+++ app/src/main/java/com/citor/app/home/searchVendor/SearchVendorActivity.kt
@@ -1,10 +1,20 @@
 package com.citor.app.home.searchVendor
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.citor.app.R
@@ -14,6 +24,7 @@ import com.citor.app.retrofit.RetrofitClient
 import com.citor.app.retrofit.response.MitraResponse
 import com.citor.app.utils.Constants
 import com.citor.app.utils.MySharedPreferences
+import com.google.android.gms.location.*
 import es.dmoral.toasty.Toasty
 import retrofit2.Call
 import retrofit2.Callback
@@ -29,11 +40,10 @@ class SearchVendorActivity : AppCompatActivity() {
     private var listVendorSearch: ArrayList<VendorItemEntity> = arrayListOf()
     private var listVendorStatusOpen: ArrayList<VendorItemEntity> = arrayListOf()
     private var listVendorStatusClose: ArrayList<VendorItemEntity> = arrayListOf()
-    private var listVendorResultSearch: ArrayList<VendorItemEntity> = arrayListOf()
-//    lateinit var client: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     companion object {
-        private const val LOCATION_PERMISSION_CODE = 101
+        private const val PERMISSION_ID = 1010
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,29 +59,11 @@ class SearchVendorActivity : AppCompatActivity() {
             super.onBackPressed()
         }
 
-//        client = LocationServices.getFusedLocationProviderClient(this)
-//        val geoCoder = Geocoder(this, Locale.getDefault())
-//        var addresses:List<Address>
-//
-//        if (ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_COARSE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            return
-//        }
-//        client.lastLocation.addOnCompleteListener {
-//            val latitude = it.result.latitude
-//            val longitude = it.result.longitude
-//            addresses = geoCoder.getFromLocation(latitude, longitude, 1)
-//
-//            val address: String = addresses[0].getAddressLine(0)
-//            Log.d("Alamat", address)
-//            searchVendorBinding.tvLocation.text = address
-//        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        requestPermission()
+
+        getLastLocation()
 
         searchVendorBinding.svLocation.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -92,7 +84,7 @@ class SearchVendorActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, areaCategory)
         searchVendorBinding.ctvArea.setAdapter(arrayAdapter)
 
-        searchVendorBinding.ctvArea.setOnItemClickListener { parent, view, position, id ->
+        searchVendorBinding.ctvArea.setOnItemClickListener { _, _, position, _ ->
             when (position) {
                 0 -> {
                     listVendorStatusClose.clear()
@@ -116,16 +108,60 @@ class SearchVendorActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if(requestCode == LOCATION_PERMISSION_CODE && grantResults.size > 0){
-//            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                getCurrentAddress()
-//            }else{
-//                Toast.makeText(this, "Permission Location Denied", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val lastLocation: Location = locationResult.lastLocation
+            Log.d("Debug:", "your last last location: " + lastLocation.longitude.toString())
+            val geoCoder = Geocoder(this@SearchVendorActivity, Locale.getDefault())
+            val address = geoCoder.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1)
+            val roadName = address[0].thoroughfare
+            val number = address[0].featureName
+            val cityName = address[0].subAdminArea
+            val currentUserLocation = "$roadName $number $cityName"
+            searchVendorBinding.tvLocation.text = currentUserLocation
+        }
+    }
+
+    private fun getLastLocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+                    val location: Location? = it.result
+                    if (location == null) {
+                        val locationRequest = LocationRequest()
+                        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                        locationRequest.interval = 10000
+                        locationRequest.fastestInterval = 5000
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+                    } else {
+                        Log.d("Debug:", "Your Location:" + location.longitude + location.latitude)
+                        val geoCoder = Geocoder(this, Locale.getDefault())
+                        val address = geoCoder.getFromLocation(location.latitude, location.longitude, 3)
+                        val roadName = address[0].thoroughfare
+                        val number = address[0].featureName
+                        val cityName = address[0].subAdminArea
+                        val currentUserLocation = "$roadName $number $cityName"
+                        searchVendorBinding.tvLocation.text = currentUserLocation
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Mohon aktifkan GPS Location Anda", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug:", "You have the Permission")
+                getLastLocation()
+            }
+        }
+    }
 
     private fun setupListItemVendor(tokenAuth: String) {
         val service = RetrofitClient().apiRequest().create(DataService::class.java)
@@ -152,7 +188,6 @@ class SearchVendorActivity : AppCompatActivity() {
             override fun onFailure(call: Call<MitraResponse>, t: Throwable) {
                 Toasty.error(this@SearchVendorActivity, R.string.try_again, Toasty.LENGTH_LONG).show()
             }
-
         })
 
     }
@@ -165,4 +200,26 @@ class SearchVendorActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkPermission(): Boolean {
+        if (
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
 }
