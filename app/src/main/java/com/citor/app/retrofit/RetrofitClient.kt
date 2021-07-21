6b1618093c9d87437cc4a1981579f552eca8251d
hnfnfl <kakaknaufal@gmail.com>
2021-07-22 06:08:44 +0700

    change endpoint url to citor.id

--- app/src/main/java/com/citor/app/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/citor/app/retrofit/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitClient {
     fun apiRequest(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://test.icaapps.online/")
+            .baseUrl("https://citor.id/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
